6f33416710a78634ddfe6ebfabd039c65326fdc4
walaazahranthedeveloper@gmail.com <walaazahranthedeveloper@gmail.com>
2024-11-01 20:37:49 +0200

    Angular, PRIMENG, JSON SERVER

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { passwordMatchValidator } from '../../shared/password-match.directive';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../interfaces/auth';
+import { MessageService } from 'primeng/api';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+ registerForm!:FormGroup;
+  constructor(
+    private fb:FormBuilder,
+    private authService:AuthService,
+    private messageService:MessageService,
+    private router:Router
+  ){
+    this.registerForm=this.fb.group({
+      fullName:['',[Validators.required,Validators.pattern(/^[a-zA-Z]+(?:[a-zA-Z]+)*$/)]],
+      email:['',[Validators.required,Validators.email]],
+      password:['',Validators.required],
+      confirmPassword:['',Validators.required]
+    },{
+      validators:passwordMatchValidator
+    })
+  }
+  get fullName(){
+    return this.registerForm.controls['fullName'];
+  }
+  get email(){
+    return this.registerForm.controls['email'];
+  }
+  get password(){
+    return this.registerForm.controls['password'];
+  }
+  get confirmPassword(){
+    return this.registerForm.controls['confirmPassword'];
+  }
+  submitDetails(){
+    const postData={...this.registerForm.value}
+    delete postData.confirmPassword;
+    this.authService.registerUser(postData as User).subscribe(
+      response => {
+        this.messageService.add({severity:'success',summary:'Success',detail:'Register Successfully'});
+        this.router.navigate(['login']);
+      },
+      error =>  {
+        this.messageService.add({severity:'error',summary:'Error',detail:'Something went wrong'});
+      },
+    )
+  }
+}
